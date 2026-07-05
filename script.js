b52236181885ccabd24b9785e0f7ecd400855bad
atultanwar198 <atultanwar83@gmail.com>
2026-07-05 11:24:41 +0530

    update add animation add project

--- script.js
+++ script.js
@@ -24,3 +24,59 @@ function updateScrollProgress() {
   // Update the width of our progress bar
   document.getElementById("scroll-bar").style.width = scrolled + "%";
 }
+
+// Work cards
+const cards = document.querySelectorAll(".card");
+const carousel = document.querySelector("#carousel");
+const dotsBox = document.querySelector("#dots");
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+let currentIndex = 2;
+
+cards.forEach((card, index) => {
+  const dot = document.createElement("button");
+  dot.classList.add("dot");
+  dotsBox.appendChild(dot);
+  card.addEventListener("click", () => {
+    showCard(index);
+  });
+  dot.addEventListener("click", () => {
+    showCard(index);
+  });
+});
+
+const dots = document.querySelectorAll(".dot");
+function showCard(index) {
+  currentIndex = index;
+  cards.forEach((card) => {
+    card.classList.remove("active");
+  });
+  dots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+  cards[currentIndex].classList.add("active");
+  dots[currentIndex].classList.add("active");
+  carousel.scrollTo({
+    left: cards[currentIndex].offsetLeft - carousel.offsetLeft - 16,
+    behavior: "smooth",
+  });
+}
+
+prevBtn.addEventListener("click", () => {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = cards.length - 1;
+  }
+  showCard(currentIndex);
+});
+
+nextBtn.addEventListener("click", () => {
+  currentIndex++;
+  if (currentIndex >= cards.length) {
+    currentIndex = 0;
+  }
+  showCard(currentIndex);
+});
+
+showCard(currentIndex);
